410d68b6274552b9ec8b56650b88fa4f0e9ba37a
Skulhunter5 <62426864+Skulhunter5@users.noreply.github.com>
2021-11-16 18:13:32 +0100

    Corrected spelling

--- src/main/java/ion/Main.java
+++ src/main/java/ion/Main.java
@@ -13,7 +13,7 @@ public class Main {
     public static File linuxDir = new File("\\\\wsl$\\Ubuntu-20.04\\shared");
     public static File inputFile = new File(linuxDir.getAbsolutePath() + "\\test.ion");
     public static File outputFile = new File(inputFile + ".asm");
-    public static File astOuputFile = new File("src/main/resources/AST-out");
+    public static File astOutputFile = new File("src/main/resources/AST-out");
 
     public static void main(String[] args) {
         String code = "";
@@ -31,7 +31,7 @@ public class Main {
         AssemblyFrontend asmFront = new AssemblyFrontend(parser);
         try {
             Utils.writeFileFromString(outputFile, asmFront.generate("nasm linux x86_64"));
-            Utils.writeFileFromString(astOuputFile, astString);
+            Utils.writeFileFromString(astOutputFile, astString);
         } catch(IOException e) {
             e.printStackTrace();
             System.exit(1);
